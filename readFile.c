42b6c905963fe194178eafe403178bf5e82d0d8c
Boondock19 <josematiasgonzalez20@gmail.com>
2023-03-23 00:21:34 -0400

    Se realiza de manera correcta la leida del CSV y su guardado

--- readFile.c
+++ readFile.c
@@ -1,10 +1,42 @@
 #include "readFile.h"
 
+
+Stop* stops[MAX_STOPS];
+
+
+void trimWord(char *str, char *str1)
+{
+    int idx = 0, j, k = 0;
+ 
+    // Iterate String until last
+    // leading space character
+    while (str[idx] == ' ' || str[idx] == '\t' || str[idx] == '\n')
+    {
+        idx++;
+    }
+ 
+    // Run a for loop from index until the original
+    // string ends and copy the content of str to str1
+    for (j = idx; str[j] != '\0'; j++)
+    {
+        str1[k] = str[j];
+        k++;
+    }
+ 
+    // Insert a string terminating character
+    // at the end of new string
+    str1[k] = '\0';
+ 
+    // Print the string with no whitespaces
+   printf("Trimmed String: %s", str1);
+}
+
 void readCsv(char* route) {
     FILE *fp;
     char row[MAX_CHAR];
     char *token;
     int hours[25] = {-1};
+    int j = 0;
 
     fp = fopen(route, "r");
     if (fp == NULL) {
@@ -37,26 +69,42 @@ void readCsv(char* route) {
         fgets(row, MAX_CHAR, fp);
         char* cod;
         char* recorrido;
-
+        Stop* stop;
         int i = 0;
         token = strtok(row, ",");
+        stop = (Stop *) malloc (sizeof(Stop));
         cod = token;
-        printf("Este es el codigo %s\n", cod);
+        strcpy(stop->cod, cod);
 
 
         token = strtok(NULL, ",");
+        
+        if (token == NULL)
+            break;
         recorrido = token;
-        printf("Este es el token %s\n", token);
+        char recorridoTrimmed [10];
+        trimWord(recorrido,recorridoTrimmed);
+        strcpy(stop->route_duration, recorridoTrimmed);
 
         while (token != NULL) {
             int users;
-            token = strtok(NULL, " ");
+
+            token = strtok(NULL, ",");
+            PassengersLoad* load;
+            load = (PassengersLoad *) malloc (sizeof(PassengersLoad));
+
             if (token == NULL)
                 break;
+            
+            load->time = hours[i]; 
             users = atoi(token);
-            printf("Este es el la cantidad de users %d\n", users);
+            load->users = users;
+            i++;
         }
-        i++;
+        stops[j] = stop;
+        j++;
+
+        printf("\n");
     }
 
 }
--- readFile.h
+++ readFile.h
@@ -4,6 +4,21 @@
 #include<stdbool.h>
 
 #define MAX_CHAR 120
+#define MAX_STOPS 40
 
+struct passengersLoad{
+    int time;
+    int users;
+}; 
+typedef struct passengersLoad PassengersLoad;
+struct stop{
+    char cod[4];
+    char nombre[20];
+    char route_duration[6];
+    PassengersLoad* load[24];
+};
+typedef struct stop Stop;
+
+char *trimString(char *str);
 void readCsv(char* route);
 
